ba785bd75b79e1a56973704ad786c909f899fab3
lasya-danda <lasyadanda4406@gmail.com>
2025-09-23 22:34:24 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+<<<<<<< HEAD
+
+=======
+import { componentTagger } from "lovable-tagger";
+>>>>>>> 63891759c5c0ec28bf1d45e341bf8eb3894b48a6
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
+<<<<<<< HEAD
   plugins: [react()],
+=======
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+>>>>>>> 63891759c5c0ec28bf1d45e341bf8eb3894b48a6
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})); remove lovable and give
